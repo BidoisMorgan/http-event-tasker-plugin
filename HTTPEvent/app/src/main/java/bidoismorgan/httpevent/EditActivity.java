4d25de6341eb8f6c7185b6ab02535118ac068329
BidoisMorgan <morganbidois@gmail.com>
2015-04-01 16:04:53 +0200

    Adding Initial Project

---
+++ HTTPEvent/app/src/main/java/bidoismorgan/httpevent/EditActivity.java
@@ -0,0 +1,15 @@
+package bidoismorgan.httpevent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class EditActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_activity);
+    }
+
+}
